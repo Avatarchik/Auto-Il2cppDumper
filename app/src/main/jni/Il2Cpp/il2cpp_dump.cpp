dc8c2fd08a3e2577517787df8382836de194fdd4
q3b5q3 <40742924+AndnixSH@users.noreply.github.com>
2022-09-26 16:14:55 +0200

    Geokar2006's bypass doesn't work in some games. Disabled for now

--- app/src/main/jni/Il2Cpp/il2cpp_dump.cpp
+++ app/src/main/jni/Il2Cpp/il2cpp_dump.cpp
@@ -382,96 +382,97 @@ void il2cpp_dump(void *handle, char *outDir) {
 
     //start dump
     LOGI("Get all assemblies");
-    //size_t size;
-    //auto assemblies = il2cpp_domain_get_assemblies(domain, &size);
-    auto assemblies = Assembly$$GetAllAssemblies();
+
+    size_t size;
+    auto assemblies = il2cpp_domain_get_assemblies(domain, &size);
+    //geokar2006's bypass
+    //auto assemblies = Assembly$$GetAllAssemblies();
+    //LOGI("Assemblies size %d", assemblies->size());
 
     std::stringstream imageOutput;
 
-    LOGI("Assemblies size %d", assemblies->size());
-    for (int i = 0; i < assemblies->size(); ++i) {
-        auto image = il2cpp_assembly_get_image((*assemblies)[i]);
-        /*for (int i = 0; i < size; ++i) {
-            auto image = il2cpp_assembly_get_image(assemblies[i]);
-            imageOutput << "// Image " << i << ": " << il2cpp_image_get_name(image) << "\n";
-        }*/
-        std::vector<std::string> outPuts;
-        if (il2cpp_image_get_class) {
-            LOGI("Version greater than 2018.3");
-            //使用il2cpp_image_get_class
-            /*for (int i = 0; i < size; ++i) {
-                auto image = il2cpp_assembly_get_image(assemblies[i]);*/
-            for (auto &assemblie: *assemblies) {
-                auto image = il2cpp_assembly_get_image(assemblie);
-                std::stringstream imageStr;
-                imageStr << "\n// Dll : " << il2cpp_image_get_name(image);
-                auto classCount = il2cpp_image_get_class_count(image);
-                for (int j = 0; j < classCount; ++j) {
-                    auto klass = il2cpp_image_get_class(image, j);
-                    auto type = il2cpp_class_get_type(const_cast<Il2CppClass *>(klass));
-                    //LOGD("type name : %s", il2cpp_type_get_name(type));
-                    auto outPut = imageStr.str() + dump_type(type);
-                    outPuts.push_back(outPut);
-                }
-            }
-        } else {
-            LOGI("Version less than 2018.3");
-            //使用反射
-            auto corlib = il2cpp_get_corlib();
-            auto assemblyClass = il2cpp_class_from_name(corlib, "System.Reflection", "Assembly");
-            auto assemblyLoad = il2cpp_class_get_method_from_name(assemblyClass, "Load", 1);
-            auto assemblyGetTypes = il2cpp_class_get_method_from_name(assemblyClass, "GetTypes", 0);
-            if (assemblyLoad && assemblyLoad->methodPointer) {
-                LOGI("Assembly::Load: %p", assemblyLoad->methodPointer);
-            } else {
-                LOGI("miss Assembly::Load");
-                return;
-            }
-            if (assemblyGetTypes && assemblyGetTypes->methodPointer) {
-                LOGI("Assembly::GetTypes: %p", assemblyGetTypes->methodPointer);
-            } else {
-                LOGI("miss Assembly::GetTypes");
-                return;
-            }
-            typedef void *(*Assembly_Load_ftn)(void *, Il2CppString *, void *);
-            typedef Il2CppArray *(*Assembly_GetTypes_ftn)(void *, void *);
-            /*for (int i = 0; i < size; ++i) {
-                auto image = il2cpp_assembly_get_image(assemblies[i]);*/
-            for (int i = 0; i < assemblies->size(); ++i) {
-                auto image = il2cpp_assembly_get_image((*assemblies)[i]);
-                std::stringstream imageStr;
-                auto image_name = il2cpp_image_get_name(image);
-                imageStr << "\n// Dll : " << image_name;
-                //LOGD("image name : %s", image->name);
-                auto imageName = std::string(image_name);
-                auto pos = imageName.rfind('.');
-                auto imageNameNoExt = imageName.substr(0, pos);
-                auto assemblyFileName = il2cpp_string_new(imageNameNoExt.c_str());
-                auto reflectionAssembly = ((Assembly_Load_ftn) assemblyLoad->methodPointer)(nullptr,
-                                                                                            assemblyFileName,
-                                                                                            nullptr);
-                auto reflectionTypes = ((Assembly_GetTypes_ftn) assemblyGetTypes->methodPointer)(
-                        reflectionAssembly, nullptr);
-                auto items = reflectionTypes->vector;
-                for (int j = 0; j < reflectionTypes->max_length; ++j) {
-                    auto klass = il2cpp_class_from_system_type((Il2CppReflectionType *) items[j]);
-                    auto type = il2cpp_class_get_type(klass);
-                    //LOGD("type name : %s", il2cpp_type_get_name(type));
-                    auto outPut = imageStr.str() + dump_type(type);
-                    outPuts.push_back(outPut);
-                }
-            }
-        }
-        auto outPath = std::string(outDir).append("/").append(GetPackageName()).append("-dump.cs");
-        LOGI("Write dump file to %s", outPath.c_str());
-
-        std::ofstream outStream(outPath);
-        outStream << imageOutput.str();
-        auto count = outPuts.size();
-        for (int i = 0; i < count; ++i) {
-            outStream << outPuts[i];
-        }
-        outStream.close();
-        LOGI("dump done!");
+    /*for (int i = 0; i < assemblies->size(); ++i) {
+        auto image = il2cpp_assembly_get_image((*assemblies)[i]);*/
+    for (int i = 0; i < size; ++i) {
+        auto image = il2cpp_assembly_get_image(assemblies[i]);
+        imageOutput << "// Image " << i << ": " << il2cpp_image_get_name(image) << "\n";
     }
+    std::vector<std::string> outPuts;
+    if (il2cpp_image_get_class) {
+        LOGI("Version greater than 2018.3");
+        //使用il2cpp_image_get_class
+        for (int i = 0; i < size; ++i) {
+            auto image = il2cpp_assembly_get_image(assemblies[i]);
+            /*for (auto &assemblie: *assemblies) {
+                auto image = il2cpp_assembly_get_image(assemblie);*/
+            std::stringstream imageStr;
+            imageStr << "\n// Dll : " << il2cpp_image_get_name(image);
+            auto classCount = il2cpp_image_get_class_count(image);
+            for (int j = 0; j < classCount; ++j) {
+                auto klass = il2cpp_image_get_class(image, j);
+                auto type = il2cpp_class_get_type(const_cast<Il2CppClass *>(klass));
+                //LOGD("type name : %s", il2cpp_type_get_name(type));
+                auto outPut = imageStr.str() + dump_type(type);
+                outPuts.push_back(outPut);
+            }
+        }
+    } else {
+        LOGI("Version less than 2018.3");
+        //使用反射
+        auto corlib = il2cpp_get_corlib();
+        auto assemblyClass = il2cpp_class_from_name(corlib, "System.Reflection", "Assembly");
+        auto assemblyLoad = il2cpp_class_get_method_from_name(assemblyClass, "Load", 1);
+        auto assemblyGetTypes = il2cpp_class_get_method_from_name(assemblyClass, "GetTypes", 0);
+        if (assemblyLoad && assemblyLoad->methodPointer) {
+            LOGI("Assembly::Load: %p", assemblyLoad->methodPointer);
+        } else {
+            LOGI("miss Assembly::Load");
+            return;
+        }
+        if (assemblyGetTypes && assemblyGetTypes->methodPointer) {
+            LOGI("Assembly::GetTypes: %p", assemblyGetTypes->methodPointer);
+        } else {
+            LOGI("miss Assembly::GetTypes");
+            return;
+        }
+        typedef void *(*Assembly_Load_ftn)(void *, Il2CppString *, void *);
+        typedef Il2CppArray *(*Assembly_GetTypes_ftn)(void *, void *);
+        for (int i = 0; i < size; ++i) {
+            auto image = il2cpp_assembly_get_image(assemblies[i]);
+            /*for (int i = 0; i < assemblies->size(); ++i) {
+                auto image = il2cpp_assembly_get_image((*assemblies)[i]);*/
+            std::stringstream imageStr;
+            auto image_name = il2cpp_image_get_name(image);
+            imageStr << "\n// Dll : " << image_name;
+            //LOGD("image name : %s", image->name);
+            auto imageName = std::string(image_name);
+            auto pos = imageName.rfind('.');
+            auto imageNameNoExt = imageName.substr(0, pos);
+            auto assemblyFileName = il2cpp_string_new(imageNameNoExt.c_str());
+            auto reflectionAssembly = ((Assembly_Load_ftn) assemblyLoad->methodPointer)(nullptr,
+                                                                                        assemblyFileName,
+                                                                                        nullptr);
+            auto reflectionTypes = ((Assembly_GetTypes_ftn) assemblyGetTypes->methodPointer)(
+                    reflectionAssembly, nullptr);
+            auto items = reflectionTypes->vector;
+            for (int j = 0; j < reflectionTypes->max_length; ++j) {
+                auto klass = il2cpp_class_from_system_type((Il2CppReflectionType *) items[j]);
+                auto type = il2cpp_class_get_type(klass);
+                //LOGD("type name : %s", il2cpp_type_get_name(type));
+                auto outPut = imageStr.str() + dump_type(type);
+                outPuts.push_back(outPut);
+            }
+        }
+    }
+    auto outPath = std::string(outDir).append("/").append(GetPackageName()).append("-dump.cs");
+    LOGI("Write dump file to %s", outPath.c_str());
+
+    std::ofstream outStream(outPath);
+    outStream << imageOutput.str();
+    auto count = outPuts.size();
+    for (int i = 0; i < count; ++i) {
+        outStream << outPuts[i];
+    }
+    outStream.close();
+    LOGI("dump done!");
 }
